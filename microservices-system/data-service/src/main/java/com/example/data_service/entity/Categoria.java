2e43d8dd1530233ddd87d9368748008c2cfccfe5
valeartola <mv.artola@alumno.um.edu.ar>
2025-09-13 10:57:51 -0300

    implementación entidades JPA

---
+++ microservices-system/data-service/src/main/java/com/example/data_service/entity/Categoria.java
@@ -0,0 +1,28 @@
+package com.example.data_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "categorias")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String nombre;
+
+    @Column(length = 500)
+    private String descripcion;
+    
+}
+
---
+++ microservices-system/data-service/src/main/java/com/example/data_service/entity/Producto.java
@@ -0,0 +1,29 @@
+package com.example.data_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "productos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Column(length = 500)
+    private String descripcion;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal precio;
+
+}
